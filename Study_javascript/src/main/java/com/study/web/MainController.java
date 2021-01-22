a0aa761726fce04f0d6831d22d857100e23b1924
hi_hoo2 <hi_hoo2@naver.com>
2021-01-22 22:54:48 +0900

    특정일자 이전에만 함수 실행 - javascript

--- Study_javascript/src/main/java/com/study/web/MainController.java
+++ Study_javascript/src/main/java/com/study/web/MainController.java
@@ -1,11 +1,13 @@
 package com.study.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
 	
-	@RequestMapping("/javascript")
+	@RequestMapping("/compareDates")
 	public String studyJavascript() {
-		return "javascript";
+		return "/compareDates/javascript";
 	}
 }
